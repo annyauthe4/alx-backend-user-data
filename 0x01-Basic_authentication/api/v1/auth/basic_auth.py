8153bdf413895ec26d945fe9ae1ec5775129e207
Oluwaseun Otetumo <annyauthe@yahoo.com>
2025-06-03 03:39:16 +0100

    Extract base 64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,8 +2,21 @@
 """Contains a basic authentication class that inherits from Auth
 """
 from api.v1.auth.auth import Auth
+import base64
+from flask import request
 
 
 class BasicAuth(Auth):
     """Inherits from Auth class"""
-    pass
+    def extract_base64_authorization_header(
+        self, authorization_header: str
+    ) -> str:
+        """Returns base64 part of the authorization header."""
+        auth_header = authorization_header
+        if auth_header is None or not isinstance(auth_header, str):
+            return None
+
+        if not auth_header.startswith('Basic '):
+            return None
+        else:
+            return auth_header[6:]
