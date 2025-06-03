b34e258c4c391a5089f0b571257fb6c5c27f5da4
Oluwaseun Otetumo <annyauthe@yahoo.com>
2025-06-03 04:36:24 +0100

    Decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 """
 from api.v1.auth.auth import Auth
 import base64
+import binascii
 from flask import request
 
 
@@ -20,3 +21,26 @@ class BasicAuth(Auth):
             return None
         else:
             return auth_header[6:]
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """
+        Returns the decoded value of a Base64 string.
+
+        Args:
+            base64_authorization_header (str):
+            The Base64 string to decode.
+
+        Returns:
+            str: The decoded UTF-8 string or None if invalid.
+        """
+        b_auth_h = base64_authorization_header
+        if b_auth_h is None or not isinstance(b_auth_h, str):
+            return None
+
+        try:
+            decoded = base64.b64decode(b_auth_h).decode('utf-8')
+            return decoded
+        except (binascii.Error, UnicodeDecodeError):
+            return None
