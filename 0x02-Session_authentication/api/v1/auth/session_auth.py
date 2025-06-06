a4da61e13ac6843da8846c146c1042ff5919b662
Oluwaseun Otetumo <annyauthe@yahoo.com>
2025-06-06 04:37:55 +0100

    Create a seesion id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,8 +4,24 @@ This module has a class which inherits from Auth and has a new
 authentication mechanism - Session authentication.
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """Defines the session authentication mechanism."""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a Session ID for a user_id
+
+        Args:
+            user_id (str): ID of the current user.
+
+        Returns:
+            A Session ID string.
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.__class__.user_id_by_session_id[session_id] = user_id
+        return session_id
