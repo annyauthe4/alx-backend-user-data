9fecbd2c0acc9567c7fd1a3acd04a16d116165e6
Oluwaseun Otetumo <annyauthe@yahoo.com>
2025-06-06 04:02:26 +0100

    Create session authentication class

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module has a class which inherits from Auth and has a new
+authentication mechanism - Session authentication.
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """Defines the session authentication mechanism."""
+    pass
