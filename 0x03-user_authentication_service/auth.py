e90b31f6172b20bd8d8fdd920299155675ab26e3
Oluwaseun Otetumo <annyauthe@yahoo.com>
2025-06-15 12:52:26 +0100

    Make change

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -162,6 +162,7 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(reset_token=reset_token)
+
         except Exception:
             raise ValueError("Invalid reset token")
 
